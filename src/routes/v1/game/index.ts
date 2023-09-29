0231564f5bbee572c3a5f2ccd8fa5be7d0bea16f
Henrique <henrique.mrcr@gmail.com>
2023-09-29 01:35:16 +0100

    websockets

--- src/routes/v1/game/index.ts
+++ src/routes/v1/game/index.ts
@@ -7,7 +7,8 @@ enum AttributeToHit {
   aura = 'aura',
   life = 'life',
   shield = 'shield',
-  resurrected = 'resurrected'
+  blocked = 'blocked',
+  talk = 'talk',
 }
 
 interface Skill {
@@ -21,6 +22,12 @@ enum Aura {
   neutral = 'neutral'
 }
 
+enum Turn {
+  night = 'night',
+  day = 'day',
+  both = 'both'
+}
+
 interface User {
   sockId: string;
   index: number;
@@ -32,12 +39,19 @@ interface User {
   online: number;
   resurrected: number;
   skillUsed: Skill | null;
+  skillCount: number;
+  skillTurn: Turn;
+  blocked: boolean;
+  talk: boolean;
+  name: string;
 }
 
 interface Room {
   users: User[];
   id: string;
   turn: number;
+  startedAt: Date | null;
+  lastTurn: number;
 }
 
 let games: Room[] = [];
@@ -54,7 +68,81 @@ function createNewUser(sockId: string, index: number) {
     online: 1,
     resurrected: 0,
     skillUsed: null,
+    skillCount: 0,
+    skillTurn: "night" as Turn,
+    blocked: false,
+    talk: true,
+    name: `Player ${index + 1}`,
   }
+}
+
+function getARoom() {
+  const roomsNotFull = games.filter(
+    (room) => room.users.length < USERS_TO_START_GAME
+  );
+
+  if (roomsNotFull.length === 0) {
+    const room = {
+      users: [],
+      id: Math.random().toString(36).substring(7),
+      turn: 0,
+      startedAt: null,
+      lastTurn: 0,
+    };
+
+    games.push(room);
+
+    return room;
+  } else {
+    return roomsNotFull[0];
+  }
+}
+
+function findUserRoom(sockId: string) {
+  return games.find(room => room.users.find(user => user.sockId === sockId));
+}
+
+function filterUsersByAura(users: User[], aura: Aura) {
+  return users.filter(user => user.aura === aura);
+}
+
+function filterUsersData(users: User[], aura: Aura) {
+  if (aura === Aura.good || aura === Aura.neutral) {
+    return users.map(user => {
+      return {
+        alive: user.alive,
+        online: user.online,
+        name: user.name,
+        index: user.index,
+        sockId: user.sockId,
+      } as User | any
+    })
+  } else {
+    return users.map(user => {
+      return {
+        aura: user.aura === Aura.good ? null : Aura.evil,
+        class: user.aura === Aura.good ? null : user.class,
+        alive: user.alive,
+        online: user.online,
+        name: user.name,
+        index: user.index,
+        sockId: user.sockId,
+      } as User | any
+    })
+  }
+}
+
+function startGame(Socket: Socket, room: Room) {
+  Socket.to(room.id).emit("start-game", room.users.map(user => ({
+      name: user.name, index: user.index, sockId: user.sockId
+    })
+  ));
+
+  room.users.map(user => {
+    Socket.emit("users-data", filterUsersData(room.users, user.aura));
+  })
+
+  room.startedAt = new Date();
 }
 
 export default function (fastify: FastifyInstance, opts: any, done: any) {
@@ -66,18 +154,22 @@ export default function (fastify: FastifyInstance, opts: any, done: any) {
 
       if (Socket.recovered) {
         // recovery was successful: socket.id, socket.rooms and socket.data were restored
-        const room = games.find(room => room.users.find(user => user.sockId === Socket.id));
+        const room = findUserRoom(Socket.id);
 
-        if (room) {
-          room.users = room.users.map(user => {
-            if (user.sockId === Socket.id) {
-              user.online = 1;
-            }
-            return user;
-          });
-
-          fastify.io.emit("users", room.users);
+        if (!room ) {
+          return Socket.disconnect(true);
         }
+
+        room.users = room.users.map(user => {
+          if (user.sockId === Socket.id) {
+            user.online = 1;
+          }
+          return user;
+        });
+
+        room.users.map(user => {
+          Socket.emit("users-data", filterUsersData(room.users, user.aura));
+        })
       } else {
         // new or unrecoverable session
       }
@@ -86,28 +178,44 @@ export default function (fastify: FastifyInstance, opts: any, done: any) {
       //   Socket.disconnect(true);
       // }
 
-      let room: Room | null = null
-
-      const roomsWithLessThan16Players = games.filter(
-        (room) => room.users.length < USERS_TO_START_GAME
-      );
-
-      if (roomsWithLessThan16Players.length === 0) {
-         room = {
-          users: [],
-          id: Socket.id,
-          turn: 0,
-        };
-
-        games.push(room);
-
-      } else {
-        room = roomsWithLessThan16Players[0];
-      }
+      let room = getARoom();
 
       const user = createNewUser(Socket.id, room.users.length);
 
       room.users.push(user);
+
+      Socket.join(room.id);
+
+      // enviando dados para o cliente
+      Socket.emit("user", user);
+      
+      Socket.to(room.id).emit("lobby", room.users.map(user => ({
+          name: user.name, index: user.index, sockId: user.sockId
+        })
+      ));
+      // fastify.io.emit("users", room.users);
+
+      if (room.users.length === USERS_TO_START_GAME) {
+        startGame(Socket, room);
+      }
+
+      Socket.on("handle-skill", (data: {
+        target: string;
+      }) => {
+        const room = games.find(room => room.users.find(user => user.sockId === Socket.id));
+
+        if (room) {
+          console.log('handle-skill', data.target);
+          room.users.map(user => {
+            Socket.emit("users-data", filterUsersData(room.users, user.aura));
+          })
+          // fastify.io.emit("users", room.users);
+        }
+      });
+
+      Socket.on("ping", () => {
+        Socket.emit("pong");
+      });
 
       Socket.on("disconnect", (data: any) => {
         console.info("Socket disconnected!", Socket.id);
@@ -126,24 +234,13 @@ export default function (fastify: FastifyInstance, opts: any, done: any) {
             games = games.filter(game => game.id !== room.id);
           }
 
-          fastify.io.emit("users", room.users);
+          room.users.map(user => {
+            Socket.emit("users-data", filterUsersData(room.users, user.aura));
+          })
+          // fastify.io.emit("users", room.users);
         }
       });
 
-      fastify.io.emit("users", room.users);
-
-      Socket.on("click-on-user", (data: {
-        target: string;
-      }) => {
-        const room = games.find(room => room.users.find(user => user.sockId === Socket.id));
-
-        if (room) {
-          console.log('click-on-user', data.target);
-
-          fastify.io.emit("users", room.users);
-        }
-        
-      });
     });
   });
 
