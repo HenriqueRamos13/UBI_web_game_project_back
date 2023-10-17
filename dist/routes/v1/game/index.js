f8ec477b75e45187c3ba9d22dd7e0c6fb85f51b3
Henrique Ramos <henrique.mrcr@gmail.com>
2023-10-17 23:07:22 +0100

    refactor

--- dist/routes/v1/game/index.js
+++ dist/routes/v1/game/index.js
@@ -39,7 +39,7 @@ exports.SocketOnEvents = exports.SocketEmitEvents = void 0;
 const client_1 = require("@prisma/client");
 const jwt = __importStar(require("jsonwebtoken"));
 const skillController_1 = __importDefault(require("./skillController"));
-const PLAYERS_TO_START_GAME = 2;
+const PLAYERS_TO_START_GAME = 3;
 var SocketEmitEvents;
 (function (SocketEmitEvents) {
     SocketEmitEvents["PONG"] = "pong";
